e1f2ea053f1442b858b8b7580ad49017c358181c
andryweerasinghe <andryweerasinghe@gmail.com>
2025-07-17 22:04:12 +0530

    Crops Reducer

---
+++ src/models/Crop.ts
@@ -0,0 +1,10 @@
+export default class Crops {
+    crop_code!: string;
+    category!: string;
+    common_name!: string;
+    img!: string;
+    scientific_name!: string;
+    season!: string;
+    field_code!: string;
+    date_added!: string;
+}
